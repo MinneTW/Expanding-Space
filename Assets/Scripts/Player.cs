59fdfe5374a8ebd4d758688adf8247f360af2b07
Alexbroking <alexbroking@gmail.com>
2018-05-17 11:35:08 +0200

    initial Unity commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    float speedWalk = 5f;
+    float x = 0;
+    
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+	    if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(x += speedWalk * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(x -= speedWalk * Time.deltaTime, 0, 0);
+        }
+    }
+}
